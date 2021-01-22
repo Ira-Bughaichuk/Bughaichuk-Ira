a56aff59dc87866004bcbfd7a036c5d6f841120a
Ira <18IraBughaichuk@ukr.net>
2021-01-22 16:59:34 +0200

    lesson28/1

--- project/js/customs.js
+++ project/js/customs.js
@@ -48,6 +48,51 @@ jQuery(document).ready(function () {
 			}
 		}
 	});
+
+	var slid2 = jQuery('#slider-baner6');/*привязуємо перемінну до id*/
+		
+	slid2.owlCarousel({
+		items: 4,
+		autoplay: true,
+		autoplayTimeout: 6000,
+		autoplayHoverPause: false,
+		smartSpeed: 1000,
+		dotsSpeed: 1000,
+		loop: true,
+		nav: fals,/**/ 
+		navText: ['', ''],
+		dots: true,/**/ 
+		mouseDrag: true,
+		margin: 30,/**/ 
+		stagePadding: 0,
+		autoWidth: false,
+		responsiveClass: true,
+		responsive:{
+			0:{
+				items: 1,
+				dots: true,
+				nav: false,
+				mouseDrag: true,
+				autoplay: false
+			},
+			479:{
+				items: 1,
+				mouseDrag: true
+			},
+			767:{
+				items: 3,/* к-сть картинок*/ 
+				mouseDrag: true
+			},
+			991:{
+				items: 4,
+				mouseDrag: true
+			},
+			1099:{
+				items: 4,
+				mouseDrag: true
+			}
+		}
+	});
 	
 	jQuery('.btn_menu').click(function(event){
 				jQuery('.nav-menu ul').toggle('slow');
